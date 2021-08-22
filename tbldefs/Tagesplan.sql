08152c88e08561819344d5bc7e5b0693965882d9
Dennis Schönig <dennis605@web.de>
2021-08-22 17:52:49 +0200

    rptTageesplan Entwurf erstellt

--- tbldefs/Tagesplan.sql
+++ tbldefs/Tagesplan.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [Tagesplan] (
   [TagesplanID] AUTOINCREMENT CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
-  [fldTagesplanDatum] DATETIME ,
   [fldTagesplanName] VARCHAR (255),
+  [fldTagesplanDatum] DATETIME ,
   [VeranstaltungTagesplan] VARCHAR 
 )
